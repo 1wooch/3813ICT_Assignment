410e02f0963082ce07ff6cc21b6e9d7aefbd86f0
Wonwoo Choi <onechoo1106@gmail.com>
2022-09-03 15:38:53 +1000

    account page

--- Frontend/assignment/src/app/account/account.component.ts
+++ Frontend/assignment/src/app/account/account.component.ts
@@ -11,9 +11,45 @@ const BACKEND_URL='http://localhost:3000';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  form:any={
+    e_username:null,
+    e_birthdate:null,
+    e_age:null,
+    e_email:null,
+    e_role:null,
+    e_pwd:null
+  }
+  
+  username=sessionStorage.getItem('username');
+  birthdate=sessionStorage.getItem('birthdate');
+  age=sessionStorage.getItem('age');
+  email=sessionStorage.getItem('email');
+  role=sessionStorage.getItem('role');
+  pwd=sessionStorage.getItem('pwd');
+
+
+
+  constructor(private httpClient:HttpClient,private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  public edit(){
+    const{e_email,e_role,e_age,e_birthdate,e_username,e_pwd}=this.form;
+
+    //console.log(e_email,e_role,e_birthdate,e_age,e_username); //working
+    sessionStorage.setItem('username',e_username);
+    sessionStorage.setItem('email',e_email);
+    sessionStorage.setItem('age',e_age);
+    sessionStorage.setItem('role',e_role);
+    sessionStorage.setItem('birthdate',e_birthdate);
+    sessionStorage.setItem('pwd',e_pwd);
+
+    
+    this.httpClient.post(BACKEND_URL+'/loginafter',{pwd:e_pwd,username:e_username,email:e_email,age:e_age,role:e_role,birthdate:e_birthdate})
+    .subscribe();
+    
+    
+
+  }
 }
